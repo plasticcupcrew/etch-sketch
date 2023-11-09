452a009a89c27a831c30f26d3b99227bf7eb8bfe
Michael Borrello <mborrello1@gmail.com>
2023-11-09 16:51:59 -0500

    this should be a mistake, will revert to previous commit

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
 const container = document.querySelector(".container");
 let row = document.createElement("div");
-row.classList.add("row");
 
 for (let i = 1; i<=16; i++) {
 
     const div = document.createElement("div");
     div.classList.add("box");
-    row.appendChild(div);
+    container.appendChild(div);
 
-    if (i % 4 === 0) {
-        container.appendChild(row);
-        row = document.createElement("div");
-        row.classList.add("row");
-    }
 }
 
 let grid = document.querySelectorAll(".box");
@@ -26,6 +20,8 @@ const input = document.querySelector("input");
 
 btn.addEventListener('click', () => createGrid());
 
+let newBox;
+
 function createGrid() {
 
     //remove existing grid
@@ -37,32 +33,16 @@ function createGrid() {
     let size = +input.value;
     if (size>100) {size = 100;}
 
-    //identify number of columns
-    let numCol;
-    if (Math.floor(Math.sqrt(size)) === Math.sqrt(size)) {
-        numCol = Math.sqrt(size);
-    } else {
-        numCol = Math.floor(Math.sqrt(size)) + 1;
-    }
-
     //create counter to keep track of how many boxes we have
     let counter = 0;
 
-    for (let i = 0; i < numCol; i++) {
-
-        row = document.createElement("div");
-        row.classList.add("row");
-
-        for (let j = 0; j < numCol; j++) {
-            const div = document.createElement("div");
-            div.classList.add("box");
-            row.appendChild(div);
-            if (++counter === size) {break;}
-        }
-
-        container.appendChild(row);
+    for (let i = 0; i < size; i++) {
         if (counter === size) {
             break;
         }
+        newBox = document.createElement("div");
+        newBox.classList.add("box");
+        container.appendChild(newBox);
+        counter++;
     }
 }
