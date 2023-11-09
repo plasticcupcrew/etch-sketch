41a0ce33887a51c34a09a48667742cddfc2e664e
Michael Borrello <mborrello1@gmail.com>
2023-11-09 16:37:53 -0500

    Create function to allow user to create new grid
    function clears the current grid and creates a new grid.
    Number of boxes in the new grid is based on the user's input.

--- script.js
+++ script.js
@@ -16,8 +16,53 @@ for (let i = 1; i<=16; i++) {
 }
 
 let grid = document.querySelectorAll(".box");
-console.log(grid[0]);
 
 for (let i = 0; i < grid.length; i++) {
     grid[i].addEventListener('mouseenter', () => grid[i].classList.add("colored"));
+}
+
+const btn = document.querySelector("button");
+const input = document.querySelector("input");
+
+btn.addEventListener('click', () => createGrid());
+
+function createGrid() {
+
+    //remove existing grid
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+
+    //record number of boxes
+    let size = +input.value;
+    if (size>100) {size = 100;}
+
+    //identify number of columns
+    let numCol;
+    if (Math.floor(Math.sqrt(size)) === Math.sqrt(size)) {
+        numCol = Math.sqrt(size);
+    } else {
+        numCol = Math.floor(Math.sqrt(size)) + 1;
+    }
+
+    //create counter to keep track of how many boxes we have
+    let counter = 0;
+
+    for (let i = 0; i < numCol; i++) {
+
+        row = document.createElement("div");
+        row.classList.add("row");
+
+        for (let j = 0; j < numCol; j++) {
+            const div = document.createElement("div");
+            div.classList.add("box");
+            row.appendChild(div);
+            if (++counter === size) {break;}
+        }
+
+        container.appendChild(row);
+        if (counter === size) {
+            break;
+        }
+    }
 }
