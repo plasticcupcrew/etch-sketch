9c21932eb5783bc11a26a145b9aad20639f97a63
Michael Borrello <mborrello1@gmail.com>
2023-11-13 11:25:28 -0500

    Clean up code
    Rename create grid function to updateGrid and break
    it doen into smaller functions that remove existing
    grid, get the number of columns, create a new grid, and
    add event listeners to all of the new boxes.

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const container = document.querySelector(".container");
+
 let row = document.createElement("div");
 row.classList.add("row");
+
+let grid = document.querySelectorAll(".box");
+const btn = document.querySelector("button");
+
+const input = document.querySelector("input");
+
+btn.addEventListener('click', () => updateGrid());
 
 for (let i = 1; i<=16; i++) {
 
@@ -15,39 +23,48 @@ for (let i = 1; i<=16; i++) {
     }
 }
 
-let grid = document.querySelectorAll(".box");
-
 for (let i = 0; i < grid.length; i++) {
     grid[i].addEventListener('mouseenter', () => grid[i].classList.add("colored"));
 }
 
-const btn = document.querySelector("button");
-const input = document.querySelector("input");
-
-btn.addEventListener('click', () => createGrid());
-
-function createGrid() {
+function updateGrid() {
 
     //remove existing grid
-    while (container.firstChild) {
-        container.removeChild(container.lastChild);
-    }
+    removeGrid();
 
-    //record number of boxes
+    //record number of boxes, max of 100
     let size = +input.value;
     if (size>100) {size = 100;}
 
     //identify number of columns
+    let numCol = getNumCol(size);
+
+    //create new grid.
+    createGrid(numCol, size);
+
+    //add event listeners to grid
+    addBoxListeners();
+
+}
+
+function removeGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+}
+
+function getNumCol(size) {
     let numCol;
     if (Math.floor(Math.sqrt(size)) === Math.sqrt(size)) {
         numCol = Math.sqrt(size);
     } else {
         numCol = Math.floor(Math.sqrt(size)) + 1;
     }
+    return numCol;
+}
 
-    //create counter to keep track of how many boxes we have
+function createGrid(numCol, size) {
     let counter = 0;
-
     for (let i = 0; i < numCol; i++) {
 
         row = document.createElement("div");
@@ -64,5 +81,13 @@ function createGrid() {
         if (counter === size) {
             break;
         }
+    }
+}
+
+function addBoxListeners() {
+    let grid = document.querySelectorAll(".box");
+
+    for (let i = 0; i < grid.length; i++) {
+        grid[i].addEventListener('mouseenter', () => grid[i].classList.add("colored"));
     }
 }
